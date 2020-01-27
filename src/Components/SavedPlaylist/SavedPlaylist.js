d65901bfd7f2e7f821dd09c91764c9afc05476f3
darkmatter999 <oliverhaack@protonmail.com>
2020-01-27 15:55:08 +0100

    Added a feature that displays a short notification when a mouseover (hover) is executed on the Show Playlist Details button

--- src/Components/SavedPlaylist/SavedPlaylist.js
+++ src/Components/SavedPlaylist/SavedPlaylist.js
@@ -5,12 +5,15 @@ class SavedPlaylist extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            togglePlaylistDetails: false
+            togglePlaylistDetails: false,
+            togglePlaylistDetailsDetails: false
         }
         this.removePlaylist = this.removePlaylist.bind(this);
         this.savePlaylist = this.savePlaylist.bind(this);
         this.renderPlaylistDetails = this.renderPlaylistDetails.bind(this);
         this.togglePlaylistDetails = this.togglePlaylistDetails.bind(this);
+        this.renderPlaylistDetailsDetails = this.renderPlaylistDetailsDetails.bind(this);
+        this.togglePlaylistDetailsDetails = this.togglePlaylistDetailsDetails.bind(this);
 
     }
     renderAction() {
@@ -27,9 +30,29 @@ class SavedPlaylist extends React.Component {
         }
     }
 
+    togglePlaylistDetailsDetails() {
+        if (this.state.togglePlaylistDetailsDetails === false) {
+            this.setState({togglePlaylistDetailsDetails: true})
+        } else if (this.state.togglePlaylistDetailsDetails === true) {
+            this.setState({togglePlaylistDetailsDetails: false})
+        }  
+    }
+
+    // display a short notification upon Mouseover
+    renderPlaylistDetailsDetails() {
+        if (this.state.togglePlaylistDetailsDetails === true && this.state.togglePlaylistDetails === false) {
+            return (
+                <p>Show playlist details</p>
+            )
+        } else if (this.state.togglePlaylistDetailsDetails === true && this.state.togglePlaylistDetails === true) {
+            return (
+                <p>Hide playlist details</p>
+            )
+        }
+    }
+
     // show some details (track name, artist) of the playlist when a button is toggled (see above)
     renderPlaylistDetails() {
-        
         const playlistDetails = [];
         if (this.state.togglePlaylistDetails) {
            for (let i = 0; i < this.props.playlist.tracks.length; i++) {
@@ -43,8 +66,6 @@ class SavedPlaylist extends React.Component {
                 }
             }
         return playlistDetails
-        
-       
     }
 
     removePlaylist() {
@@ -58,24 +79,27 @@ class SavedPlaylist extends React.Component {
 
     render() {
         return (
-            <div>
-            <div className="PlaylistMain">
-                <div className="Playlist-information">
-                    <h3>{this.props.playlist.name}</h3>
-                    <p>{this.props.playlist.tracks.length} {this.props.playlist.tracks.length === 1 ? 'song' : 'songs'} &nbsp;&nbsp;
-                        <button
-                            className="Playlist-details" onClick={this.togglePlaylistDetails}><img className='Arrow-image' src="/images/arrow-down.jpg" />
-                        </button>
-                    </p>
+            <div className="Outer-div">
+                <div className="PlaylistMain">
+                    <div className="Playlist-information">
+                        <h3>{this.props.playlist.name}</h3>
+                        <p>{this.props.playlist.tracks.length} {this.props.playlist.tracks.length === 1 ? 'song' : 'songs'} &nbsp;&nbsp;
+                            <button
+                                className="Playlist-details" onClick={this.togglePlaylistDetails}
+                                onMouseOver={this.togglePlaylistDetailsDetails}
+                                onMouseOut={this.togglePlaylistDetailsDetails}><img className='Arrow-image' src="/images/arrow-down.jpg" />
+                            </button>
+                        </p>
+                    </div>
+                    {this.renderAction()}
+                    <button className="Playlist-action" onClick={this.savePlaylist}>
+                        <img className='Spotify-image' src="/images/spotify.jpg" />
+                    </button>
+                    <br /> <br /> 
                 </div>
-                {this.renderAction()}
-                <button className="Playlist-action" onClick={this.savePlaylist}>
-                    <img className='Spotify-image' src="/images/spotify.jpg" />
-                </button>
-                <br /> <br />
-                
-            </div>
-            <div><p>{this.renderPlaylistDetails()}</p></div>
+                <div><p>{this.renderPlaylistDetailsDetails()}</p></div>
+                <br /> 
+                <div><p>{this.renderPlaylistDetails()}</p></div>
             </div>
         )
     }
